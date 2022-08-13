add7dda96353006ce2426b173714f26e9a4ebd85
sudhir kumar <Sudhirkmr40@gmail.com>
2022-08-13 15:35:03 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import numpy as np
+l = [1,2,3,4,5,6]
+print(l)
+
+
+l1 = [4,5,6,7,"sudhir", 34.54,True]
+print(l1)
+x=np.array(l1)
+print(x)
+
+a1 =np.array([[1,2,3,4],[455,5,42,4]])
+print(a1)
+
+a2  = np.array([[12,3,4,4],[4,234,5,53]])
+print(a2)
+
+print(a1.ndim)
+print(a2.ndim)
+
