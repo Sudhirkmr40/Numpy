0e5b174e6ddada068d9d484723e9d943036e347c
sudhir kumar <Sudhirkmr40@gmail.com>
2022-08-23 16:38:57 +0530

    initial commit

--- main.py
+++ main.py
@@ -29,6 +29,40 @@ print(a1.shape)
 print(np.random.randint(2,10))
 
 
+a4 = np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(a4)
+print(a4.ndim)
+print(a4.shape)
+
+x2 = np.random.randint(2,50,(2,3,4))
+
+print(x2)
+
+x3 = np.random.randn(2,3)
+print(x3)
+
+x3 = np.random.rand(2,3)
+print(x3)
+
+x4 = np.random .rand(4,4)
+print(x4)
+
+x5 = x4.reshape(1,2)
+print(x5)
+
+
+y = x6[2:4]
+print(y)
+
+
+
+
+
+
+
+l1 = [4,5,6,7,"sudhir", 34.54,True]
+z = l1[::-1]
+print(z)
 
 
 
